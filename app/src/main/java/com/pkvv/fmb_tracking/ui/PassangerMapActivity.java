20291e9ce5bf8a755302ec21851eb602d936f80f
VinayVst <vinaytigadi1998@gmail.com>
2020-04-14 16:59:02 +0530

    Activities created

---
+++ app/src/main/java/com/pkvv/fmb_tracking/ui/PassangerMapActivity.java
@@ -0,0 +1,16 @@
+package com.pkvv.fmb_tracking.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.pkvv.fmb_tracking.R;
+
+public class PassangerMapActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_passanger_map);
+    }
+}
